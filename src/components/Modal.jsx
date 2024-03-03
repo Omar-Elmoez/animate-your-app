dfb25ba5bc105539bedcdfe88bd5230ec139a620
Omar Elmoez <omarelmoez2000@gmail.com>
2024-03-03 15:12:10 +0200

    apply framer motion on challenges page components

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,25 @@
-import { createPortal } from 'react-dom';
-
+import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        open
+        className="modal"
+        variants={{
+          hidden: { y: 30, opacity: 0 },
+          visible: { y: 0, opacity: 1 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        transition={{ duration: 0.4 }}
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
