753965102b3274b8fb24d8a3d302f4243e80d52b
Omar Elmoez <omarelmoez2000@gmail.com>
2024-03-04 10:05:58 +0200

    apply imperative animation (useAnimate Hook)

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -1,5 +1,5 @@
 import { useContext, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, stagger, useAnimate } from "framer-motion";
 import { ChallengesContext } from "../store/challenges-context.jsx";
 import Modal from "./Modal.jsx";
 import images from "../assets/images.js";
@@ -16,6 +16,8 @@ export default function NewChallenge({ onDone }) {
     setSelectedImage(image);
   }
 
+  const [scope, animate] = useAnimate();
+
   function handleSubmit(event) {
     event.preventDefault();
     const challenge = {
@@ -31,6 +33,11 @@ export default function NewChallenge({ onDone }) {
       !challenge.deadline.trim() ||
       !challenge.image
     ) {
+      animate(
+        "input, textarea",
+        { x: [-10, 0, 10, 0], border: "1px solid red" },
+        { type: "spring", duration: .5, delay: stagger(.04) }
+      );
       return;
     }
 
@@ -40,70 +47,74 @@ export default function NewChallenge({ onDone }) {
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
         <motion.div
-          variants={{ visible: { transition: { staggerChildren: 0.2 } } }}
+          variants={{ visible: { transition: { staggerChildren: 0.6 } } }}
         >
-          <motion.p
-            variants={{
-              hidden: { y: -50, opacity: 0 },
-              visible: { y: 0, opacity: 1 },
-            }}
-            transition={{ type: "spring" }}
-            exit={{ y: 0, opacity: 1 }}
+          <motion.div
+            variants={{ visible: { transition: { staggerChildren: 0.2 } } }}
           >
-            <label htmlFor="title">Title</label>
-            <input ref={title} type="text" name="title" id="title" />
-          </motion.p>
-
-          <motion.p
-            variants={{
-              hidden: { y: -50, opacity: 0 },
-              visible: { y: 0, opacity: 1 },
-            }}
-            transition={{ type: "spring" }}
-            exit={{ y: 0, opacity: 1 }}
-          >
-            <label htmlFor="description">Description</label>
-            <textarea ref={description} name="description" id="description" />
-          </motion.p>
-
-          <motion.p
-            variants={{
-              hidden: { y: -50, opacity: 0 },
-              visible: { y: 0, opacity: 1 },
-            }}
-            transition={{ type: "spring" }}
-            exit={{ y: 0, opacity: 1 }}
-          >
-            <label htmlFor="deadline">Deadline</label>
-            <input ref={deadline} type="date" name="deadline" id="deadline" />
-          </motion.p>
-        </motion.div>
-
-        <motion.ul
-          id="new-challenge-images"
-          variants={{ visible: { transition: { staggerChildren: 0.05 } } }}
-        >
-          {images.map((image) => (
-            <motion.li
-              key={image.alt}
+            <motion.p
               variants={{
-                hidden: { opacity: 0, scale: 0.5 },
-                visible: {
-                  opacity: 1,
-                  scale: 1,
-                },
+                hidden: { y: -50, opacity: 0 },
+                visible: { y: 0, opacity: 1 },
               }}
-              exit={{ opacity: 1, scale: 1 }}
               transition={{ type: "spring" }}
-              onClick={() => handleSelectImage(image)}
-              className={selectedImage === image ? "selected" : undefined}
+              exit={{ y: 0, opacity: 1 }}
             >
-              <img {...image} />
-            </motion.li>
-          ))}
-        </motion.ul>
+              <label htmlFor="title">Title</label>
+              <input ref={title} type="text" name="title" id="title" />
+            </motion.p>
+
+            <motion.p
+              variants={{
+                hidden: { y: -50, opacity: 0 },
+                visible: { y: 0, opacity: 1 },
+              }}
+              transition={{ type: "spring" }}
+              exit={{ y: 0, opacity: 1 }}
+            >
+              <label htmlFor="description">Description</label>
+              <textarea ref={description} name="description" id="description" />
+            </motion.p>
+
+            <motion.p
+              variants={{
+                hidden: { y: -50, opacity: 0 },
+                visible: { y: 0, opacity: 1 },
+              }}
+              transition={{ type: "spring" }}
+              exit={{ y: 0, opacity: 1 }}
+            >
+              <label htmlFor="deadline">Deadline</label>
+              <input ref={deadline} type="date" name="deadline" id="deadline" />
+            </motion.p>
+          </motion.div>
+
+          <motion.ul
+            id="new-challenge-images"
+            variants={{ visible: { transition: { staggerChildren: 0.05 } } }}
+          >
+            {images.map((image) => (
+              <motion.li
+                key={image.alt}
+                variants={{
+                  hidden: { opacity: 0, scale: 0.5 },
+                  visible: {
+                    opacity: 1,
+                    scale: 1,
+                  },
+                }}
+                exit={{ opacity: 1, scale: 1 }}
+                transition={{ type: "spring" }}
+                onClick={() => handleSelectImage(image)}
+                className={selectedImage === image ? "selected" : undefined}
+              >
+                <img {...image} />
+              </motion.li>
+            ))}
+          </motion.ul>
+        </motion.div>
 
         <p className="new-challenge-actions">
           <button type="button" onClick={onDone}>
